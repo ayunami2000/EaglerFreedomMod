d66be43e5aab143f94ca0536be4812ea86a925d7
ayunami2000 <spwilliamsiam@gmail.com>
2022-04-11 00:31:55 -0400

    allow anyone to /opme + optimize for lower specs

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_setl.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_setl.java
@@ -7,17 +7,22 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Sets everyone's Worldedit block modification limit to 500.", usage = "/<command>")
+@CommandParameters(description = "Sets everyone's Worldedit block modification limit to x.", usage = "/<command>")
 public class Command_setl extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.adminAction(sender.getName(), "Setting everyone's Worldedit block modification limit to 500.", true);
+        int lim = 500;
+        try
+        {
+            lim = args.length == 0 ? 500 : Math.max(1, Integer.parseInt(args[0]));
+        } catch (NumberFormatException ignored) {}
+        TFM_Util.adminAction(sender.getName(), "Setting everyone's Worldedit block modification limit to " + lim + ".", true);
         TFM_WorldEditBridge web = TFM_WorldEditBridge.getInstance();
         for (final Player player : server.getOnlinePlayers())
         {
-            web.setLimit(player, 500);
+            web.setLimit(player, lim);
         }
         return true;
     }
